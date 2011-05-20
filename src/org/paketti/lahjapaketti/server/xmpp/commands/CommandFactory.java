4e8223ba0c71c3f72a0822a63e5842d3376151f4
Henri Nybom <henri.nybom@gmail.com>
2011-05-20 09:54:29 +0300

    removed StringUtils

--- src/org/paketti/lahjapaketti/server/xmpp/commands/CommandFactory.java
+++ src/org/paketti/lahjapaketti/server/xmpp/commands/CommandFactory.java
@@ -1,6 +1,5 @@
 package org.paketti.lahjapaketti.server.xmpp.commands;
 
-import org.apache.commons.lang.StringUtils;
 import org.paketti.lahjapaketti.server.xmpp.commands.weather.WeatherCommand;
 
 import com.google.appengine.api.xmpp.Message;
@@ -8,7 +7,7 @@ import com.google.appengine.api.xmpp.Message;
 public class CommandFactory {
 
 	public static XmppCommand build(final Message message) {
-		if (StringUtils.isNotEmpty(message.getBody()) && message.getBody().toLowerCase().startsWith("s‰‰")) {
+		if (message.getBody() != null && message.getBody().toLowerCase().startsWith("s‰‰")) {
 			return new WeatherCommand(message);
 		}
 
